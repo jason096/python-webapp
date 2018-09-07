d21552c0a805bd31290bc6858412146709b86d00
jason <603052164@qq.com>
2018-09-07 14:58:14 +0800

    add database scripts

--- www/app.py
+++ www/app.py
@@ -4,17 +4,17 @@ import time
 import asyncio
 import os
 from datetime import datetime
-from aiohttp import webbrowser
+from aiohttp import web
 
 logging.basicConfig(level=logging.INFO)
 
 
 def index(request):
-    return web.response(body=b'<h1>Awesome</h1>')
+    return web.Response(body=b'<h1>Awesome</h1>')
 
 
 def init(loop):
-    app = web.application(loop=loop)
+    app = web.Application(loop=loop)
     app.router.add_route("GET","/",index)
     srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
     logging.info('server started at http://127.0.0.1:9000...')
